612529e2e223c204b63988b48773d7f51e344815
pbertelini <45045935+pbertelini@users.noreply.github.com>
2024-10-27 00:30:58 +0000

    correções de formatação de codigo

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 treinamento dev
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 function Home() {
-
-    return <h1>my home page new version</h1>
-
+  return <h1>my home page new version</h1>;
 }
 
 export default Home;
